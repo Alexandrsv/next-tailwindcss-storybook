10108c44cbfcd4b7e7bf61be1fc8a9a6ddee55e4
Александр <zbc@mail.ru>
2022-08-11 22:10:44 +0300

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,21 @@
+// @ts-check
+import * as NextImage from 'next/image';
+import '../styles/globals.css';
+
+const OriginalNextImage = NextImage.default;
+
+// eslint-disable-next-line no-import-assign
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    return {
+      ...config,
+      plugins: [...config.plugins, new NodePolyfillPlugin({})],
+    };
+  },
 };
 
 module.exports = nextConfig;
